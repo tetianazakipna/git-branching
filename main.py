3b83996e4b24793eaad47fd1542a1f62f9ca3341
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-05-06 10:20:46 -0700

    Switched to Zakipna

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #creating a simple function
 def my_function(fname, lname):
-  print(fname + lname)
+  print(f"{fname} {lname}")
 
-my_function("Trenisha", "Goslee")
-my_function("Deshaunte", "Goldsmith")
+my_function("Tetiana", "Zakipna")
+my_function("Iurii", "Zakipniy")
 
 
